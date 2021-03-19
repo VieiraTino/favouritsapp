ff29a2f62bb2feadd4eef9f7589083860b8f0c78
TiVieira <tinovieira27@gmail.com>
2021-03-19 17:42:24 +0000

    search movies data filter

--- src/utils/findmovies.js
+++ src/utils/findmovies.js
@@ -9,12 +9,27 @@ const findmovies = (movie, callback) => {
         } else if (body.results.length === 0) {
             callback("Não foi possível encontrar resultados para o filme '" + movie + "'.", undefined);
         } else {
-            // recebo mais páginas aqui... reenviar tudo
-            callback(undefined, body);
+            const response = [];
+
+            body.results.forEach((movie) => {
+                response.push({
+                    movie: movie.original_title,
+                    idOnTMDB: movie.id,
+                    poster: "https://www.themoviedb.org/t/p/w1280" + movie.poster_path
+                });
+            });
+
+            callback(undefined, response);
         }
     })
 }
 
+// const movieSearch = (movie) => {
+//     response.push({movie.poster_path, movie.imdb_id, movie.original_title })
+// }
+
+
+// obj.poster_path + obj.imdb_id + obj.original_title
 // poster https://www.themoviedb.org/t/p/w1280/XWz5SS5g5mrNEZjv3FiGhqCMOQ.jpg
 
 module.exports = findmovies;
