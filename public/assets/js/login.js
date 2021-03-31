26aa245bc062d460f6c23fb8eb60a70739505418
TiVieira <tinovieira27@gmail.com>
2021-03-31 17:12:44 +0100

    frond end structure and login page

---
+++ public/assets/js/login.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document
+        .getElementById('loginForm')
+        .addEventListener('submit', login);
+})
+
+
+function login(event) {
+    event.preventDefault();
+    let formData = new FormData(event.target)
+
+    const body = {}
+    for(let key of formData.keys()){ 
+        body[key] = formData.get(key)
+    }
+
+    const url = 'http://localhost:3000/api/login'
+    
+
+    let data = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    }
+
+    fetch(url, data)
+        .then((res) => res.json())
+        .then((data) => {
+            if(data.token){
+                window.location = 'http://localhost:3000/favourites';
+            } else {
+                alert('Username ou password incorreta');
+            }
+        })
+}
