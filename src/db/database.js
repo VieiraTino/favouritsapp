59533154d76d61b8bd55c4b0e6b531fa1872c8d0
TiVieira <tinovieira27@gmail.com>
2021-04-02 17:50:06 +0100

    heroku secret from BD

--- src/db/database.js
+++ src/db/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionURL = 'mongodb+srv://favouritesApp:w334r4fQg5kdrbN3@cluster0.5el1z.mongodb.net/favouritesapp';
+const connectionURL = process.env.mongodb_connection;
 
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
